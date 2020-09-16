ce7bb71fc5053e3de80d269e1e3cb4525aeb9fcf
Oscar Wellner <o.wellner@telfort.nl>
2020-09-17 00:18:06 +0200

    Removed unnecessary test and refactored the app component test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
