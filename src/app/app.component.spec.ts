59b82cde1832c97eaca42bb8e294bd47d6fdaa42
Oscar Wellner <o.wellner@telfort.nl>
2020-09-17 00:36:12 +0200

    Refactored e2e tests as well as the unit tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
-  let component: AppComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,17 +21,36 @@ describe('AppComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-
-    component = fixture.componentInstance;
   });
 
+  function getTestingObjects(): {
+    component: AppComponent,
+    debugElement: DebugElement,
+    element: HTMLElement,
+  } {
+    return {
+      component: fixture.componentInstance,
+      debugElement: fixture.debugElement,
+      element: fixture.debugElement.nativeElement,
+    };
+  }
+
   it('should create the app', () => {
+    const { component } = getTestingObjects();
 
     expect(component).toBeTruthy();
   });
 
   it(`should have as title 'femalert-app'`, () => {
+    const { component } = getTestingObjects();
 
     expect(component.title).toEqual('femalert-app');
+  });
+
+  // todo should be removed once the application has been expanded on.
+  it('should render text in the HTML element', () => {
+    const { element } = getTestingObjects();
+
+    expect(element.querySelector('p').innerText).toBe('FemAlert app is running');
   });
 });
