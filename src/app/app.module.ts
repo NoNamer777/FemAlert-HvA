c464315584d42301131cadf4533e3f4043d21c5b
Oscar Wellner <o.wellner@telfort.nl>
2020-10-01 12:45:13 +0200

    spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { NgModule } from '@angular/core';
-
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
