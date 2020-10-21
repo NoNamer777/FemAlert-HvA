cbfe47a23ee3009357ee6d514076b934fff3d451
Oscar Wellner <o.wellner@telfort.nl>
2020-10-21 17:58:15 +0200

    Improved and simplyfied Navbar fixed position space  conditioning

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { ReportsService } from '../../services/reports.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers: [],
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private reportsService: ReportsService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  @HostBinding('hidden')
+  get hidden(): boolean {
+    return this.reportsService.isCreatingReport;
   }
-
 }
