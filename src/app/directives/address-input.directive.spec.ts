a7c5f0326ebea1185ba1db4b76625cb215439ee8
Oscar Wellner <o.wellner@telfort.nl>
2020-11-02 12:21:08 +0100

    Disabled tests for location-picker component and address-input directive

--- src/app/directives/address-input.directive.spec.ts
+++ src/app/directives/address-input.directive.spec.ts
@@ -1,8 +1,1 @@
-import { AddressInputDirective } from './address-input.directive';
-
-describe('AddressInputDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AddressInputDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+describe('AddressInputDirective', () => {});
