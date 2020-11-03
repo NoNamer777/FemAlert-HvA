a2e618155e19f7708fa3b87ea41e2a4555b9350a
abdullahtalhaoui <abdullah11@live.nl>
2020-11-03 11:36:16 +0100

    tests confirmation componeny

--- src/app/components/content/confirmation/confirmation.component.spec.ts
+++ src/app/components/content/confirmation/confirmation.component.spec.ts
@@ -1,10 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ConfirmationComponent } from './confirmation.component';
 
 describe('ConfirmationComponent', () => {
   let component: ConfirmationComponent;
   let fixture: ComponentFixture<ConfirmationComponent>;
+  let router: Router;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,6 +23,15 @@ describe('ConfirmationComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  it('should stop button send to home page ', () => {
+    const button: HTMLButtonElement = element.querySelector('#btn-stop-report');
+    const navigationSpy = spyOn(router, 'navigate');
+
+    button.click();
+
+    expect(navigationSpy).toHaveBeenCalledWith([ '/home' ]);
+  });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
