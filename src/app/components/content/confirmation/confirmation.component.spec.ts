7c9fcf1f1baa57e240b6b03d2a9a0ea1cc912630
abdullahtalhaoui <abdullah11@live.nl>
2020-11-03 12:04:09 +0100

    Refactored tests for changes

--- src/app/components/content/confirmation/confirmation.component.spec.ts
+++ src/app/components/content/confirmation/confirmation.component.spec.ts
@@ -2,27 +2,43 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
 import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
 
 import { ConfirmationComponent } from './confirmation.component';
 
-describe('ConfirmationComponent', () => {
-  let component: ConfirmationComponent;
+fdescribe('ConfirmationComponent', () => {
   let fixture: ComponentFixture<ConfirmationComponent>;
-  let router: Router;
+  let component: ConfirmationComponent;
   let element: HTMLElement;
+
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConfirmationComponent ]
+      imports: [
+        FontAwesomeTestingModule,
+        RouterTestingModule.withRoutes([]),
+      ],
+      declarations: [
+        ConfirmationComponent,
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA,
+      ],
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ConfirmationComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
+    element = fixture.debugElement.nativeElement;
+
+    router = TestBed.inject(Router);
+
     fixture.detectChanges();
   });
+
   it('should stop button send to home page ', () => {
     const button: HTMLButtonElement = element.querySelector('#btn-stop-report');
     const navigationSpy = spyOn(router, 'navigate');
@@ -30,10 +46,5 @@ describe('ConfirmationComponent', () => {
     button.click();
 
     expect(navigationSpy).toHaveBeenCalledWith([ '/home' ]);
-  });
-
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
   });
 });
