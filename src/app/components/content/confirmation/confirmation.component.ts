3ef20093dca62e4c0d40fac77d928d2cdb5f1eb2
abdullahtalhaoui <abdullah11@live.nl>
2020-11-03 11:17:33 +0100

    Stop button added

--- src/app/components/content/confirmation/confirmation.component.ts
+++ src/app/components/content/confirmation/confirmation.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.scss']
 })
-export class ConfirmationComponent implements OnInit {
 
-  constructor() {}
+
+export class ConfirmationComponent implements OnInit {
+ iconQuit = faTimes;
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  onStop(): void {
+      this.router.navigate(['/home']);
+  }
 
 }
