cf01922f2b9ff0552c1d7ab1d315d769e49a7a27
abdullahtalhaoui <abdullah11@live.nl>
2020-11-11 00:36:09 +0100

    created about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { PartnerComponent } from './components/content/partner/partner.component
 import { LoginComponent } from './components/content/login/login.component';
 import { EmailMoreInfoDialogComponent } from './components/content/question/email-more-info-dialog/email-more-info-dialog.component';
 import { ConfirmationComponent } from './components/content/confirmation/confirmation.component';
+import { AboutComponent } from './components/content/about/about.component';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { ConfirmationComponent } from './components/content/confirmation/confirm
     EmailMoreInfoDialogComponent,
     LoginComponent,
     ConfirmationComponent,
+    AboutComponent,
   ],
   imports: [
     AppRoutingModule,
