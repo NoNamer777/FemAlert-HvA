0c60fbbd088545d81e27e4dee0f26824733f8e60
Oscar Wellner <47724489+NoNamer777@users.noreply.github.com>
2020-11-13 11:43:50 +0100

    Added coments to the location picker component.

--- src/app/components/content/location-picker/location-picker.component.ts
+++ src/app/components/content/location-picker/location-picker.component.ts
@@ -11,6 +11,7 @@ import { RapportsService } from '../../../services/rapports.service';
 import { Rapport } from '../../../models/Rapport';
 import { Address } from '../../../models/Address';
 
+/** Animation for when the address is set and displayed or removed. */
 const addressSetAnimation = trigger('showHide', [
   transition(':enter', [
     style({ height: 0, opacity: 0 }),
@@ -34,16 +35,21 @@ const addressSetAnimation = trigger('showHide', [
 })
 export class LocationPickerComponent implements OnInit {
 
+  /** Reference to the Google Maps element */
   @ViewChild(GoogleMap, { static: true }) map: GoogleMap;
 
+  /** Marker of the set address. */
   marker: google.maps.Marker = new google.maps.Marker({
     visible: false,
   });
 
+  /** The current zoom level. */
   zoom = 16;
 
+  /** A variable reference to the center of the map. */
   center: google.maps.LatLngLiteral = null;
 
+  /** The Google Map configuration. */
   options: google.maps.MapOptions = {
     zoomControl: false,
     disableDoubleClickZoom: true,
@@ -54,10 +60,13 @@ export class LocationPickerComponent implements OnInit {
     streetViewControl: false,
   };
 
+  /** The current address, if any. */
   address: string = null;
 
+  /** Result of the searched terms. */
   placeResult: google.maps.places.PlaceResult = null;
 
+  /** Fontawesome icon for the close button. */
   closeIcon = faTimes;
 
   constructor(
@@ -68,6 +77,7 @@ export class LocationPickerComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // Get the current position of the device, if allowed.
     navigator.geolocation.getCurrentPosition((position: Position) => {
       this.center = {
         lat: position.coords.latitude,
@@ -76,6 +86,7 @@ export class LocationPickerComponent implements OnInit {
       this.changeDetectionRef.markForCheck();
     });
 
+    // Get the location from the Rapport that is already in session.
     if (this.rapportsService.rapport != null) {
       this.address = this.rapportsService.rapport.address.formattedAddress;
       (document.documentElement.querySelector('input#address-input') as HTMLInputElement).value =
@@ -84,6 +95,7 @@ export class LocationPickerComponent implements OnInit {
       const geoCoder = new google.maps.Geocoder();
       const request = { address: this.address } as google.maps.GeocoderRequest;
 
+      // Gets the name of the business registered on the address, if present.
       geoCoder.geocode(request, (geocoderResults: google.maps.GeocoderResult[]) => {
         (geocoderResults[0] as any as google.maps.places.PlaceResult).name =
           this.rapportsService.rapport.address.businessName;
@@ -93,30 +105,37 @@ export class LocationPickerComponent implements OnInit {
     }
   }
 
+  /** Stops the creation of the rapport. */
   onPrevious(): void {
-    this.router.navigate(['/home']).then(() => this.rapportsService.isCreatingRapport = false);
+    this.router.navigate(['/home']);
+    
+    this.rapportsService.isCreatingRapport = false;
   }
 
+  /** Continues to the Rapport form. */
   onNext(): void {
-    this.router.navigate(['/formulier']).then(() => {
-      const rapport =  new Rapport();
+    this.router.navigate(['/formulier']);
+    
+    // Instantiate a new Rapport object, provided with the set Address location.
+    const rapport =  new Rapport();
+    rapport.address = new Address(this.placeResult.formatted_address, this.placeResult.name);
 
-      rapport.address = new Address(this.placeResult.formatted_address, this.placeResult.name);
-
-      this.rapportsService.rapport = rapport;
-    });
+    this.rapportsService.rapport = rapport;
   }
 
+  /** Gets the address street and number if present in the Address. */
   get addressStreetAndNumber(): string {
     return `${this.getAddressComponent(['route'])} ${this.getAddressComponent(['street_number'])}`;
   }
 
+  /** Gets the address postal code and city / region if present in the Address. */
   get addressPostalCodeAndCity(): string {
     return `${this.getAddressComponent(['postal_code'])} ${this.getAddressComponent(['locality', 'political'])}`;
   }
 
+  /** Handles Address changes. */
   getAddressOnChange(placeResult: google.maps.places.PlaceResult): void {
-
+    // Don't change the address when there is no result and hide the marker.
     if (placeResult.address_components == null) {
       this.placeResult = null;
       this.marker.setOptions({
@@ -128,18 +147,20 @@ export class LocationPickerComponent implements OnInit {
     this.placeResult = placeResult;
     this.address = placeResult.formatted_address;
 
+    // Center the map on the Address Geolocation.
     this.center = {
       lat: placeResult.geometry.location.lat(),
       lng: placeResult.geometry.location.lng()
     };
+    
     this.marker.setPosition(this.center);
     this.marker.setOptions({
       visible: true,
     });
-
     this.changeDetectionRef.detectChanges();
   }
 
+  /** Finds a single Address Component value from the Address Components. */
   private getAddressComponent(keys: string[]): string {
     let addressComponent = '';
 
