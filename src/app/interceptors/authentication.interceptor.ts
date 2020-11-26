cd4a9527e237376a47df3500f2c8c9fa702a8ec5
Bradley <b.buurmeester.tmi@gmail.com>
2020-11-26 12:26:01 +0100

    added auth interceptor for request

--- src/app/interceptors/authentication.interceptor.ts
+++ src/app/interceptors/authentication.interceptor.ts
@@ -11,29 +11,30 @@ import { AuthenticateService } from '../services/authenticate.service';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
+  AUTH_KEY = 'Authorization';
 
   constructor(private  authenticateService: AuthenticateService) {}
 
   /**
-   * Intercepts login request to get Authentication token
-   * Intercepts Todo
+   * Intercepts login response to get Authentication token
+   * Intercepts request if token is not null add authorization header to request
    * @param request is request to intercept
    * @param next is http handler
    */
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (this.authenticateService.token != null){
+      request.headers.append(this.AUTH_KEY, this.authenticateService.token);
+    }
+
     return next.handle(request).pipe(map((response: HttpEvent<any>) => {
       if (response instanceof HttpResponse) {
-        if (response.headers.get('Authorization') != null){
-          let token = response.headers.get('Authorization');
+        if (response.headers.get(this.AUTH_KEY) != null){
+          let token = response.headers.get(this.AUTH_KEY);
           token = token.replace('Bearer ', '');
           this.authenticateService.token = token;
         }
       }
-      if (response instanceof  HttpRequest) {
-        if (response.method === 'GET'){
-          // Todo: add token to request if auth
-        }
-      }
+
       return response;
     }));
   }
