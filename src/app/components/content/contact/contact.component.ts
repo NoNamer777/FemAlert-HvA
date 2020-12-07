c282e490344e535cc838a1d8e80dd2bdc1ad0ac2
abdullahtalhaoui <abdullah11@live.nl>
2020-12-07 22:18:22 +0100

    onStopbutton + Text contact page

--- src/app/components/content/contact/contact.component.ts
+++ src/app/components/content/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { RapportsService } from '../../../services/rapports.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  iconQuit = faTimes;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private rapportsService: RapportsService,
+  ) {}
 
   ngOnInit(): void {
+  }
+
+  /** Handles going back to the Home Page, thus stopping the process of filling in the form. */
+
+  onStop(): void {
+    this.router.navigate(['/home']);
+
+    this.rapportsService.isCreatingRapport = false;
   }
 
 }
