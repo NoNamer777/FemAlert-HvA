67f4b4cf2a4d39bf8e6547c677c595596a00fd72
abdullahtalhaoui <abdullah11@live.nl>
2020-12-09 22:15:37 +0100

    contact page layout and send button

--- src/app/components/content/contact/contact.component.ts
+++ src/app/components/content/contact/contact.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { RapportsService } from '../../../services/rapports.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { EmailMoreInfoDialogComponent } from '../question/email-more-info-dialog/email-more-info-dialog.component';
 
 @Component({
   selector: 'app-contact',
@@ -11,9 +14,19 @@ import { RapportsService } from '../../../services/rapports.service';
 export class ContactComponent implements OnInit {
   iconQuit = faTimes;
 
+  /** The questions form */
+ contactForm = new FormGroup({
+    events: new FormControl([]),
+    name: new FormControl(),
+    companyName: new FormControl(),
+    question: new FormControl(),
+    email: new FormControl(null, [ Validators.required, Validators.email]),
+  });
+
   constructor(
     private router: Router,
     private rapportsService: RapportsService,
+    private dialog: MatDialog,
   ) {}
 
   ngOnInit(): void {
@@ -25,6 +38,18 @@ export class ContactComponent implements OnInit {
     this.router.navigate(['/home']);
 
     this.rapportsService.isCreatingRapport = false;
+  }
+
+  /** Opens the dialog that provides information about why we need the email address of an User. */
+  onMIEmail(): void {
+    this.dialog.open(EmailMoreInfoDialogComponent, {
+      hasBackdrop: true,
+      closeOnNavigation: true,
+      position: {
+        top: '40%',
+        left: '10%',
+      },
+    });
   }
 
 }
