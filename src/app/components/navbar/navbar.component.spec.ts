ada9914bb2c2e625c92662c798fcf8bad1de676c
Bradley <b.buurmeester.tmi@gmail.com>
2021-01-19 12:54:55 +0100

    created new test

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -4,11 +4,13 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NavbarComponent } from './navbar.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('NavbarComponent', () => {
   let fixture: ComponentFixture<NavbarComponent>;
   let component: NavbarComponent;
   let element: HTMLElement;
+  let router: Router;
 
   let mockHttpClient: HttpTestingController;
 
@@ -20,7 +22,7 @@ describe('NavbarComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        NavbarComponent,
+        NavbarComponent
       ],
     })
     .compileComponents();
@@ -30,10 +32,12 @@ describe('NavbarComponent', () => {
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.debugElement.componentInstance;
     element = fixture.debugElement.nativeElement;
+    router = TestBed.inject(Router);
 
     mockHttpClient = TestBed.inject(HttpTestingController);
     document.documentElement.style.width = '700px';
 
+    router.navigate(['']);
     fixture.detectChanges();
   });
 
@@ -56,27 +60,45 @@ describe('NavbarComponent', () => {
     expect(collapsableContent.classList.contains('collapse')).toBe(false);
   });
 
-  // it('should toggle collapsed on anchor click', async () => {
-  //   const collapseBtn: HTMLButtonElement = element.querySelector('button.navbar-toggler');
-  //   const collapsableContent: HTMLElement = element.querySelector('#navbarCollapse');
-  //   const anchorElement: HTMLElement = element.querySelector(`a[routerlink='/faq']`);
-  //
-  //   collapseBtn.click();
-  //   fixture.detectChanges();
-  //
-  //   await fixture.whenStable();
-  //   await fixture.whenRenderingDone();
-  //
-  //   expect(collapsableContent.classList.contains('collapse')).toBe(false);
-  //
-  //   anchorElement.click();
-  //   fixture.detectChanges();
-  //
-  //   await fixture.whenStable();
-  //   await fixture.whenRenderingDone();
-  //
-  //   expect(collapsableContent.classList.contains('show')).toBe(false);
-  // });
+  it('should hide navbar when url includes /rapporteren', () => {
+    expect(component.hidden).toBeFalsy();
+
+    spyOnProperty(router, 'url').and.returnValue('/rapporteren/locatie');
+    fixture.detectChanges();
+
+    expect(component.hidden).toBeTruthy();
+  });
+
+  it('should hide navbar when url includes /partner', () => {
+    expect(component.hidden).toBeFalsy();
+
+    spyOnProperty(router, 'url').and.returnValue('/partner/members');
+    fixture.detectChanges();
+
+    expect(component.hidden).toBeTruthy();
+  });
+
+  it('should toggle collapsed on anchor click', async () => {
+    const collapseBtn: HTMLButtonElement = element.querySelector('button.navbar-toggler');
+    const collapsableContent: HTMLElement = element.querySelector('#navbarCollapse');
+    // const anchorElement: HTMLElement = element.querySelector(`a[routerlink='/faq']`); <------ moet veranderen
+
+    collapseBtn.click();
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+    await fixture.whenRenderingDone();
+
+    expect(collapsableContent.classList.contains('collapse')).toBe(false);
+
+    // anchorElement.click();
+    fixture.detectChanges();
+
+    await fixture.whenStable();
+    await fixture.whenRenderingDone();
+
+    expect(collapsableContent.classList.contains('show')).toBe(false);
+  });
 
   // it('should not toggle collapsed when document is to wide to be toggleable.', () => {
   //   const faqLink: HTMLAnchorElement = element.querySelector(`a[routerLink='/faq']`);
