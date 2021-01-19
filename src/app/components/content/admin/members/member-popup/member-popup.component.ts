0f3348333034dbfaf549ae79d738298e43d44664
Bradley <b.buurmeester.tmi@gmail.com>
2021-01-19 23:54:19 +0100

    added popup component

---
+++ src/app/components/content/admin/members/member-popup/member-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-popup',
+  templateUrl: './member-popup.component.html',
+  styleUrls: ['./member-popup.component.scss']
+})
+export class MemberPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
