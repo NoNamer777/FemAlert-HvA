ea307c433dd07056c85dc05bdecca295231a6ba3
abdullahtalhaoui <abdullah11@live.nl>
2021-01-20 02:12:59 +0100

    test contact confirmation component

--- src/app/components/content/contact/contact-confirmation/contact-confirmation.component.spec.ts
+++ src/app/components/content/contact/contact-confirmation/contact-confirmation.component.spec.ts
@@ -2,15 +2,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactConfirmationComponent } from './contact-confirmation.component';
 import { Router } from '@angular/router';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { FontAwesomeTestingModule } from '@fortawesome/angular-fontawesome/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ContactConfirmationComponent', () => {
   let component: ContactConfirmationComponent;
   let fixture: ComponentFixture<ContactConfirmationComponent>;
   let router: Router;
   let element: HTMLElement;
+  let mockHttpClient: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        FontAwesomeTestingModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'home', redirectTo: '' },
+        ]),
+      ],
       declarations: [ ContactConfirmationComponent ]
     })
     .compileComponents();
@@ -19,13 +30,15 @@ describe('ContactConfirmationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ContactConfirmationComponent);
     component = fixture.componentInstance;
+
+    element = fixture.debugElement.nativeElement;
+
+    router = TestBed.inject(Router);
+    mockHttpClient = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-  it('should stop button send to home page ', () => {
+  it('should s to home page ', () => {
     const button: HTMLButtonElement = element.querySelector('#btn-stop-report');
     const navigationSpy = spyOn(router, 'navigate');
 
